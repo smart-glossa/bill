cdaf0acc046c19210cf4920228bb5d59b9d6087f
Product Reviewer <admin@smartglossa.com>
2016-11-17 21:55:05 +0530

    Corrected error on PrintPreview

--- bill/src/main/webapp/js/list.js
+++ bill/src/main/webapp/js/list.js
@@ -7,7 +7,8 @@ function addNewLine() {
 			+ "<input type=text class='quantity'>"
 			+ "<input type=text class='pcost' readonly>"
 			+ "<input type=text class='lineTotal' value=0 readonly>"
-			+ "<img class='nextLine' alt='next' width='25px' height='25px' src='" + nextURL + "'/>";
+			+ "<img class='nextLine' alt='next' width='25px' height='25px' src='"
+			+ nextURL + "'/>";
 
 	$(".billing")[0].appendChild(div);
 	calculateBillAmount();
@@ -25,19 +26,20 @@ function calculateBillAmount() {
 function balanceAmount() {
 	var sum = $("h2").text();
 	var balance = 0;
-	if($("#cash").val().trim()==""){
+	if ($("#cash").val().trim() == "") {
 		$("#balance").val("");
-	}else{
-	var cash = $("#cash").val();
-	balance = cash - sum;
-	$("#balance").val(balance);
+	} else {
+		var cash = $("#cash").val();
+		balance = cash - sum;
+		$("#balance").val(balance);
 	}
 
 }
 function displayProducts() {
 	var url = "/bill/bill?operation=getAllProduct";
 	var imgURL = "images/deleteButton.jpg";
-	$.ajax({
+	$
+			.ajax({
 				url : url,
 				type : 'POST'
 			})
@@ -51,7 +53,9 @@ function displayProducts() {
 									+ array[i].productId + "</td>";
 							query += "<td>" + array[i].name + "</td>";
 							query += "<td>" + array[i].cost + "</td>";
-							query += "<td> <img class='deleteProduct' src='" + imgURL + "' width='25px' height='25px'/></td></tr>"
+							query += "<td> <img class='deleteProduct' src='"
+									+ imgURL
+									+ "' width='25px' height='25px'/></td></tr>"
 						}
 						query += "</table>"
 						$(".displayAll")[0].innerHTML = query;
@@ -62,26 +66,25 @@ function displayProducts() {
 
 }
 
-
-function calculateLineTotal(div){
-	 var a = div.children(".quantity").val();
-    var b = div.children(".pcost").val();
-    var c = a * b;
-    div.children(".lineTotal").val(c);
-    calculateBillAmount();
-    balanceAmount();
+function calculateLineTotal(div) {
+	var a = div.children(".quantity").val();
+	var b = div.children(".pcost").val();
+	var c = a * b;
+	div.children(".lineTotal").val(c);
+	calculateBillAmount();
+	balanceAmount();
 }
-
 
 function checkAndRemoveDuplicate(div) {
 	var duplicate = false;
 	var currentPidValue = div.children(".pid").val();
 	var currentQuantity = parseInt(div.children(".quantity").val());
 	var parentSiblings = div.siblings();
-	for (var i=0; i< parentSiblings.length; i++){
-		if(currentPidValue === $(parentSiblings[i]).children(".pid").val()) {
-			var siblingQuantity = parseInt($(parentSiblings[i]).children(".quantity").val());
-			var sum= currentQuantity + siblingQuantity;
+	for (var i = 0; i < parentSiblings.length; i++) {
+		if (currentPidValue === $(parentSiblings[i]).children(".pid").val()) {
+			var siblingQuantity = parseInt($(parentSiblings[i]).children(
+					".quantity").val());
+			var sum = currentQuantity + siblingQuantity;
 			$(parentSiblings[i]).children(".quantity").val(sum);
 			calculateLineTotal($(parentSiblings[i]));
 			duplicate = true;
@@ -89,20 +92,16 @@ function checkAndRemoveDuplicate(div) {
 		}
 	}
 	return duplicate;
-	}
-function printPreview(){
-	var div = document.createElement("div");
-	div.className = 'printPreview';
-	var table = "<table>"
-	    table += "<tr><th>Serial Number</th><th>Product Name</th><th>Quantity</th><th>cost</th><th>Total<th></tr>"
-	    table += "</table>";	
-	$(".printPreview").innerHTML = table;
-	//body.appendchild(div);
-	 var printWindow = window.open('', '', 'height=400,width=800');
-     //printWindow.document.write('<html><head><title>BILLING</title>');
-     //printWindow.document.write('</head><body >');
-    // printWindow.document.write('<div class="printPreview"></div>');
-     //printWindow.document.write('</body></html>');
-     //printWindow.document.close();
-     $(".printPreview").print();
+}
+function printPreview() {
+	var printWindow = window.open('', '', 'height=400,width=800');
+	printWindow.document.write('<html><head><title>BILLING</title>');
+	printWindow.document.write('</head><body >');
+	printWindow.document.write('<div>');
+	printWindow.document.write("<table>");
+	printWindow.document.write("<tr><th>Serial Number</th><th>Product Name</th><th>Quantity</th><th>cost</th><th>Total<th></tr>");
+	printWindow.document.write("</table></div>");
+	printWindow.document.write('</body></html>');
+	printWindow.document.close();
+	printWindow.focus();
 }
