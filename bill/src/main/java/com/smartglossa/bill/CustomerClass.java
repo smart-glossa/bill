69f807808bf805b42b42c16fe662e32313c9ff77
tamilmani <tamilmani@smartglossa>
2016-12-27 11:35:57 +0530

    change file

--- bill/src/main/java/com/smartglossa/bill/CustomerClass.java
+++ bill/src/main/java/com/smartglossa/bill/CustomerClass.java
@@ -1,12 +1,10 @@
 package com.smartglossa.bill;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -20,8 +18,7 @@ public class CustomerClass {
 
 	}
 
-	public void cusAdd(int cid, String cname, String caddr, int cphno)
-			throws ClassNotFoundException, SQLException {
+	public void cusAdd(int cid, String cname, String caddr, int cphno) throws ClassNotFoundException, SQLException {
 		try {
 			String query = "insert into customer(customerId,name,address,phonenumber)values(" + cid + ",'" + cname
 					+ "','" + caddr + "'," + cphno + ")";
@@ -29,21 +26,16 @@ public class CustomerClass {
 		} finally {
 			closeConnection();
 		}
-
 	}
 
-	public void cusUpdate(int cid, String cname, String caddr, int cphno)
-			throws ClassNotFoundException, SQLException {
+	public void cusUpdate(int cid, String cname, String caddr, int cphno) throws ClassNotFoundException, SQLException {
 		try {
 			String query = "update customer set name=' " + cname + " ',address=' " + caddr + " ',phonenumber=" + cphno
 					+ " where customerId=" + cid;
 			stat.execute(query);
-
 		} finally {
 			closeConnection();
-
 		}
-
 	}
 
 	public void cusDelete(int cid) throws ClassNotFoundException, SQLException {
@@ -53,7 +45,6 @@ public class CustomerClass {
 		} finally {
 			closeConnection();
 		}
-
 	}
 
 	public JSONObject cusOne(int id) throws ClassNotFoundException, SQLException {
@@ -65,18 +56,16 @@ public class CustomerClass {
 				result.put("name", rs.getString(2));
 				result.put("address", rs.getString(3));
 				result.put("phonenumber", rs.getInt(4));
-
 			}
 		} finally {
 			closeConnection();
 		}
-
-		return null;
-
+		return result;
 	}
-	public JSONArray cusAll() throws ClassNotFoundException,SQLException {
-		JSONArray result=new JSONArray();
-		try{
+
+	public JSONArray cusAll() throws ClassNotFoundException, SQLException {
+		JSONArray result = new JSONArray();
+		try {
 			String query = "select * from customer";
 			ResultSet res = stat.executeQuery(query);
 			while (res.next()) {
@@ -87,18 +76,49 @@ public class CustomerClass {
 				result.put(get);
 			}
 
-		}
-		finally{
+		} finally {
 			closeConnection();
-			
 		}
 		return result;
-		
+
 	}
+
+	public JSONArray cusale(int cuid) throws ClassNotFoundException, SQLException {
+		JSONArray result = new JSONArray();
+		try {
+			String query = "select saleId from customerbill where customerId=" + cuid;
+			rs = stat.executeQuery(query);
+			while (rs.next()) {
+				int saleId = rs.getInt("saleId");
+				String queryy = "select * from salemetadata,salelineitems,salepayment where salemetadata.saleId="
+						+ saleId + " AND salelineitems.saleId =" + saleId + " AND salepayment.saleId =" + saleId;
+				rs = stat.executeQuery(queryy);
+				while (rs.next()) {
+					JSONObject obj = new JSONObject();
+					obj.put("billDate", rs.getDate("billDate"));
+					obj.put("vat", rs.getFloat("vat"));
+					obj.put("discount", rs.getFloat("discount"));
+					obj.put("billTotal", rs.getFloat("billTotal"));
+					obj.put("saleLineId", rs.getInt("saleLineId"));
+					obj.put("productId", rs.getInt("productId"));
+					obj.put("quantity", rs.getFloat("quantity"));
+					obj.put("cost", rs.getFloat("cost"));
+					obj.put("payId", rs.getInt("payId"));
+					obj.put("payDate", rs.getDate("payDate"));
+					obj.put("paidAmount", rs.getFloat("paidAmount"));
+					result.put(obj);
+				}
+			}
+		} finally {
+			closeConnection();
+		}
+		return result;
+	}
+
 	private void openConnection() throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.jdbc.Driver");
-		con = DriverManager.getConnection("jdbc:mysql://" + BillConstants.MYSQL_SERVER + "/" + BillConstants.DATABASE
-				+ BillConstants.USERNAME + BillConstants.PASSWORD);
+		con = DriverManager.getConnection("jdbc:mysql://" + BillConstants.MYSQL_SERVER + "/" + BillConstants.DATABASE,
+				BillConstants.USERNAME, BillConstants.PASSWORD);
 		stat = con.createStatement();
 	}
 
@@ -114,5 +134,4 @@ public class CustomerClass {
 		}
 
 	}
-
 }
--- bill/src/main/java/com/smartglossa/bill/CustomerServlet.java
+++ bill/src/main/java/com/smartglossa/bill/CustomerServlet.java
@@ -2,16 +2,10 @@
 package com.smartglossa.bill;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -40,7 +34,6 @@ public class CustomerServlet extends HttpServlet {
 			} catch (Exception e) {
 				result.put("status", 0);
 				e.printStackTrace();
-
 			}
 			response.getWriter().print(result);
 		} else if (operation.equals("cusUpdate")) {
@@ -56,7 +49,6 @@ public class CustomerServlet extends HttpServlet {
 			} catch (Exception e) {
 				result.put("status", 0);
 				e.printStackTrace();
-
 			}
 			response.getWriter().print(result);
 		} else if (operation.equals("cusDelete")) {
@@ -69,7 +61,6 @@ public class CustomerServlet extends HttpServlet {
 			} catch (Exception e) {
 				result.put("status", 0);
 				e.printStackTrace();
-
 			}
 			response.getWriter().print(result);
 		} else if (operation.equals("cusOne")) {
@@ -77,77 +68,37 @@ public class CustomerServlet extends HttpServlet {
 			JSONObject result = new JSONObject();
 			try {
 				CustomerClass cus = new CustomerClass();
-				cus.cusOne(id);
-
-			}
-
-			catch (Exception e) {
+				result = cus.cusOne(id);
+			} catch (Exception e) {
 				result.put("status", 0);
 				e.printStackTrace();
-
 			}
 			response.getWriter().print(result);
 		} else if (operation.equals("cusAll")) {
-			JSONArray result= new JSONArray();
+			JSONArray result = new JSONArray();
 			try {
-				CustomerClass cus=new CustomerClass();
-				cus.cusAll();
-				
-				
+				CustomerClass cus = new CustomerClass();
+				result = cus.cusAll();
 			} catch (Exception e) {
-				JSONObject get=new JSONObject();
+				JSONObject get = new JSONObject();
 				get.put("status", 0);
 				result.put(get);
-				
 				e.printStackTrace();
-
 			}
 			response.getWriter().print(result);
 		} else if (operation.equals("cusale")) {
 			int cuid = Integer.parseInt(request.getParameter("customerId"));
 			JSONArray result = new JSONArray();
-
 			try {
-				Class.forName("com.mysql.jdbc.Driver");
-				Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bill", "root", "root");
-				Statement stat = con.createStatement();
-				String query = "select saleId from customerbill where customerId=" + cuid;
-				ResultSet rs = stat.executeQuery(query);
-				while (rs.next()) {
-					int saleId = rs.getInt("saleId");
-					Class.forName("com.mysql.jdbc.Driver");
-					Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bill", "root", "root");
-					Statement state = conn.createStatement();
-					String queryy = "select * from salemetadata,salelineitems,salepayment where salemetadata.saleId="
-							+ saleId + " AND salelineitems.saleId =" + saleId + " AND salepayment.saleId =" + saleId;
-					ResultSet res = state.executeQuery(queryy);
-					while (res.next()) {
-						JSONObject obj = new JSONObject();
-						obj.put("billDate", res.getDate("billDate"));
-						obj.put("vat", res.getFloat("vat"));
-						obj.put("discount", res.getFloat("discount"));
-						obj.put("billTotal", res.getFloat("billTotal"));
-						obj.put("saleLineId", res.getInt("saleLineId"));
-						obj.put("productId", res.getInt("productId"));
-						obj.put("quantity", res.getFloat("quantity"));
-						obj.put("cost", res.getFloat("cost"));
-						obj.put("payId", res.getInt("payId"));
-						obj.put("payDate", res.getDate("payDate"));
-						obj.put("paidAmount", res.getFloat("paidAmount"));
-						result.put(obj);
-
-					}
-				}
-
+				CustomerClass cus = new CustomerClass();
+				result = cus.cusale(cuid);
 			} catch (Exception e) {
 				JSONObject obj = new JSONObject();
 				obj.put("status", "0");
 				result.put(obj);
 				e.printStackTrace();
-
 			}
 			response.getWriter().print(result);
-
 		}
 	}
 }
