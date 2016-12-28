a96669ed6dc2e9077e96c1848d3bb33d160e1e74
deepikabu <deepika@smartglossa.com>
2016-12-28 10:06:00 -0800

    change file

--- bill/src/main/webapp/js/dealer.js
+++ bill/src/main/webapp/js/dealer.js
@@ -1,19 +1,20 @@
 $(document).ready(function(){
-	$(document).click('#image',function(){
+	$('#image').on('click',function(){
 		var dropdown="";
-	
 		dropdown += "<ul>";
-		dropdown += "<li>Product><\/li>";
+		dropdown += "<li>Product<\/li>";
 		dropdown += "<li>Dealer<\/li>";
 		dropdown += "<li>Customer<\/li>";
 		dropdown += "<li>Purchase<\/li>";
 		dropdown += "<li>Sales<\/li>";
 		dropdown += "<li>Expenses<\/li>";
 		dropdown += "<\/ul>";
-		
 		$('.menu')[0].innerHTML=dropdown;
-       
-
+		
+		$('.menu').css("display","block");
+		
+	},function(){
+		$(".menu").css("display","none");
 	});
 	
 });
