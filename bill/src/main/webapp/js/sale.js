b28f59f594c2ce3de04fc4ba0db581d5da81b66b
deepikabu <deepika@smartglossa.com>
2017-01-28 17:07:14 -0800

    change in js purchase ,sale

--- bill/src/main/webapp/js/sale.js
+++ bill/src/main/webapp/js/sale.js
@@ -17,39 +17,33 @@ function salemeta() {
     salemeta += "<\/tr>";
     salemeta += "<\/table>";
     salemeta += "<\/div>";
-    $('#lists ')[0].innerHTML = salemeta;
-}
-
-function saleline() {
-    var saleline = "";
-    saleline += "<div class=\"saleline\">";
-    saleline += "<h2> Add SaleLineItem <\/h2>";
-    saleline += "<table>";
-    saleline += "<tr>";
-    saleline += "<td>SaleLineId*:<\/td>";
-    saleline += "<td><input type=\"text\" id=\"saleLineId\"><\/td>";
-    saleline += "<\/tr>";
-    saleline += "<tr>";
-    saleline += "<td>SaleId*:<\/td>";
-    saleline += "<td><input type=\"text\"id=\"saleId\"><\/td>";
-    saleline += "<\/tr>";
-    saleline += "<tr>";
-    saleline += "<td>ProductId*:<\/td>";
-    saleline += "<td><input type=\"text\" placeholder=\"productId\" id=\"productId\"><\/td>";
-    saleline += "<\/tr>";
-    saleline += "<tr>";
-    saleline += "<td>Quantity*:<\/td>";
-    saleline += "<td><input type=\"text\" placeholder=\"Quantity\" id=\"quantity\"><\/td>";
-    saleline += "<\/tr>";
-    saleline += "<td>Cost*:<\/td>";
-    saleline += "<td><input type=\"text\" placeholder=\"Cost\" id=\"cost\"><\/td>";
-    saleline += "<\/tr>";
-    saleline += "<td><input type=\"button\" value=\"Add\" id=\"addline\">";
-    saleline += "<input type=\"button\" value=\"Update\" id=\"updateSaleLineItem\"><\/td>";
-    saleline += "<\/tr>";
-    saleline += "<\/table>";
-    saleline += "<\/div>";
-    $('#lists')[0].innerHTML = saleline;
+    salemeta += "<div class=\"saleline\">";
+    salemeta += "<h2> Add SaleLineItem <\/h2>";
+    salemeta += "<table>";
+    salemeta += "<tr>";
+    salemeta += "<td>SaleId*:<\/td>";
+    salemeta += "<td><input type=\"text\"id=\"saleId\"><\/td>";
+    salemeta += "<\/tr>";
+    salemeta += "<tr>";
+    salemeta += "<td>ProductId*:<\/td>";
+    salemeta += "<td><input type=\"text\" placeholder=\"productId\" id=\"productId\"><\/td>";
+    salemeta += "<\/tr>";
+    salemeta += "<tr>";
+    salemeta += "<td>Quantity*:<\/td>";
+    salemeta += "<td><input type=\"text\" placeholder=\"Quantity\" id=\"quantity\"><\/td>";
+    salemeta += "<\/tr>";
+    salemeta += "<td>Cost*:<\/td>";
+    salemeta += "<td><input type=\"text\" placeholder=\"Cost\" id=\"cost\"><\/td>";
+    salemeta += "<\/tr>";
+    salemeta += "<td>Total*:<\/td>";
+    salemeta += "<td><input type=\"text\" placeholder=\"Total\" id=\"cost\"><\/td>";
+    salemeta += "<\/tr>";
+    salemeta += "<td><input type=\"button\" value=\"Add\" id=\"addline\">";
+    salemeta += "<input type=\"button\" value=\"Update\" id=\"updateSaleLineItem\"><\/td>";
+    salemeta += "<\/tr>";
+    salemeta += "<\/table>";
+    salemeta += "<\/div>";
+    $('#lists')[0].innerHTML = salemeta;
 }
 function calculateBillAmount() {
     var count = $("div.lineProduct").length;
